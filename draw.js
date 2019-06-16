b7a58f1590ce4defcc72ff6438d64fbc83a0f9d3
calvintts <calvinteytaisoon@gmail.com>
2019-06-16 01:35:06 -0700

    first commit

---
+++ draw.js
@@ -0,0 +1,36 @@
+var scene = new THREE.Scene();
+scene.background = new THREE.Color(0xebe6e6)
+var camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight , 0.1, 150);
+camera.position.z=80;
+camera.position.y=2;
+camera.lookAt(0,0,0);
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight/2 );
+document.getElementById("banner").appendChild( renderer.domElement );
+var loader = new THREE.FontLoader();
+loader.load( 'asset/helvetiker_bold.typeface.json', function ( font ) {
+	var geometry = new THREE.TextGeometry( 'CALVIN TEY', {
+		font: font,
+	     size: 18,
+	     height: 1,
+	     curveSegments: 12,
+	     bevelEnabled: true,
+	     bevelThickness: 0.5,
+	     bevelSize: 0.25,
+	     bevelOffset: 0,
+	     bevelSegments: 5
+	} );
+	geometry.center();
+  var material = new THREE.MeshBasicMaterial( { color: 0xb6363b } );
+  var txt = new THREE.Mesh( geometry, material );
+  scene.add( txt );
+  function animate() {
+  	requestAnimationFrame( animate );
+  	renderer.render( scene, camera );
+    txt.rotation.y +=0.01;
+		renderer.setSize( window.innerWidth, window.innerHeight/2 );
+
+  }
+  animate();
+
+} );
